056368737cd9b983389150b12524bc827fd857e7
Dezormeaux Thierry <thierry.dezormeaux@volvo.com>
2017-06-15 22:35:22 +0200

    recupere heure par UDP et reset à minuit

--- StationMeteo/StationMeteo.ino
+++ StationMeteo/StationMeteo.ino
@@ -31,6 +31,8 @@
 
 #include <Adafruit_BMP085.h> // Library for BMP180 "Adafruit_BMP085 library v1.0.0" 
 
+#include <WiFiUdp.h>
+
 //for LED status
 #include <Ticker.h>
 Ticker ticker;
@@ -72,6 +74,25 @@ int MinTemperature = +80;
 // Connect SDA to i2c data - D2 on NodeMCU
 
 Adafruit_BMP085 bmp180_sensor;
+
+//Variable pour récupération de l'heure par serveur basé sur l'exemple Udp NTP Client
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+unsigned long lastUDPRequest = 0 ;
+unsigned long delayUDPRequest = 60000; // 60s = 1min - délai de requête UDP
+
+unsigned long lastHours = 0; //Save last hours
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
 
 /***********  setup, to run once  ***************************************************/
 void setup() {
@@ -128,10 +149,82 @@ void setup() {
 
   ThingSpeak.begin(client);
   attachInterrupt(2,dotest,RISING); //Input D4
+
+  //Pour récupérer date et heure
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
 }
 
 /***********  main, run repeatedly **************************************************/
 void loop() {
+
+  // On récupère l'heure
+  if( (millis()- lastUDPRequest) > delayUDPRequest) 
+  {
+    lastUDPRequest = millis();
+    //get a random server from the pool
+    WiFi.hostByName(ntpServerName, timeServerIP); 
+  
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    // wait to see if a reply is available
+    delay(1000);
+
+    int cb = udp.parsePacket();
+    if (!cb) {
+      Serial.println("no packet yet");
+    }
+    else {
+      Serial.print("packet received, length=");
+      Serial.println(cb);
+      // We've received a packet, read the data from it
+      udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  
+      //the timestamp starts at byte 40 of the received packet and is four bytes,
+      // or two words, long. First, esxtract the two words:
+  
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      // combine the four bytes (two words) into a long integer
+      // this is NTP time (seconds since Jan 1 1900):
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      Serial.print("Seconds since Jan 1 1900 = " );
+      Serial.println(secsSince1900);
+  
+      // now convert NTP time into everyday time:
+      Serial.print("Unix time = ");
+      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      const unsigned long seventyYears = 2208988800UL;
+      // subtract seventy years:
+      unsigned long epoch = secsSince1900 - seventyYears;
+      // print Unix time:
+      Serial.println(epoch);
+  
+  
+      // print the hour, minute and second:
+      Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+      Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+      Serial.print(':');
+      if ( ((epoch % 3600) / 60) < 10 ) {
+        // In the first 10 minutes of each hour, we'll want a leading '0'
+        Serial.print('0');
+      }
+      Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+      Serial.print(':');
+      if ( (epoch % 60) < 10 ) {
+        // In the first 10 seconds of each minute, we'll want a leading '0'
+        Serial.print('0');
+      }
+      Serial.println(epoch % 60); // print the second
+
+      if((((epoch  % 86400L) / 3600) == 0) && (lastHours == 23))
+      {
+        resetMinMax();
+      }
+      lastHours = (epoch  % 86400L) / 3600;
+    }   
+  }
 
   //READ DATA
   Serial.print("DHT11, \t");
@@ -151,16 +244,10 @@ void loop() {
       Serial.print("Unknown error,\t");
       break;
   }
-  // DISPLAY DATA
+
   humidity_DHT = DHT.humidity;
   temperature_DHT = DHT.temperature;
   currentTemperature = bmp180_sensor.readTemperature();
-  Serial.print(humidity_DHT, 1);
-  Serial.print("%,\t");
-  Serial.print(temperature_DHT, 1);
-  Serial.print("*C,\t bmp180: ");
-  Serial.print(currentTemperature, 1);
-  Serial.println("*C");
 
   //Check if temperature MIN or MAX
   if(MaxTemperature < currentTemperature)
@@ -171,6 +258,19 @@ void loop() {
   {
     MinTemperature = currentTemperature;
   }
+
+  // DISPLAY DATA
+  Serial.print(humidity_DHT, 1);
+  Serial.print("%,\t");
+  Serial.print(temperature_DHT, 1);
+  Serial.print("*C,\t bmp180: ");
+  Serial.print(currentTemperature, 1);
+  Serial.print("*C, Min: ");
+  Serial.print(MinTemperature, 1);
+  Serial.print("*C, Max: ");
+  Serial.print(MaxTemperature, 1);
+  Serial.println("*C");
+
   
 /*  //Display on LCD I2C
   lcd.setCursor(2,1);
@@ -256,12 +356,14 @@ void loop() {
     lastWriteThingSpeak = millis();
     ThingSpeak.setField(1,currentTemperature);
     ThingSpeak.setField(2,humidity_DHT);
+    ThingSpeak.setField(3,MinTemperature);
+    ThingSpeak.setField(4,MaxTemperature);
 
     // Write the fields that you've set all at once.
     ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);  
   }
 
-  delay(2000); // Wait 2 secondes before new loop
+  delay(10000); // Wait 10 secondes before new loop
 }
 
 /***********  for LED status  **************************************************/
@@ -326,3 +428,36 @@ void configModeCallback (WiFiManager *myWiFiManager) {
 
   Serial.println(myWiFiManager->getConfigPortalSSID());
 }
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
+/***********  Reset temperature Min Max **************************************************/
+void resetMinMax()
+{
+  MaxTemperature = currentTemperature;
+  MinTemperature = currentTemperature;
+}
+
